8f7c9010a8dc2744a21135669927ad7740a3a05f
Daniel Min <danielm710@gmail.com>
2020-09-07 01:34:56 -0700

    Updates

---
+++ precache-manifest.1f00390957da2350e88553ed20598b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7651278998ac8d905cd146c3a4aac9bf",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "b7b0b9e905dfeb8eb37c",
+    "url": "/cv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3a9776dc335d8c99f2a",
+    "url": "/cv/static/js/2.3e88142a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv/static/js/2.3e88142a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b0b9e905dfeb8eb37c",
+    "url": "/cv/static/js/main.9bbe8ee1.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
